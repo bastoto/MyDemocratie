37b78b3e9791d3ad4215d3b1086c9fc43a40bfa9
bastien berardi <bastien.berardi@gmail.com>
2025-12-05 22:57:50 +0100

    fix: update ActivityPanel to use correct vote verification library

--- src/components/ActivityPanel.tsx
+++ src/components/ActivityPanel.tsx
@@ -1,8 +1,7 @@
 'use client'
 
 import { useState, useEffect } from 'react'
-import { getStoredPassphrase } from '@/lib/vote-encryption'
-import { verifyApproveRejectVote, verifyDurationVote } from '@/lib/encryption'
+import { getStoredPassphrase, verifyApproveRejectVote, verifyDurationVote } from '@/lib/vote-encryption'
 
 interface ActivityPanelProps {
     title: string
